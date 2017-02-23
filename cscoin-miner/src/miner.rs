5245870d8f76d124130aff095d31ce3c29bdef58
Alexis Grondin <agrondin1@hotmail.com>
2017-02-22 19:50:33 -0500

    Added stuff

--- cscoin-miner/src/miner.rs
+++ cscoin-miner/src/miner.rs
@@ -1,6 +1,41 @@
 extern crate mersenne_twister;
 extern crate rand;
+extern crate sha2;
 use mersenne_twister::MersenneTwister;
+use sha2::{Sha256, Digest};
 use rand::{Rng, SeedableRng};
+
+
+struct miner{
+    rng: MersenneTwister;
+    lastSeed: u64;
+}
+
+impl miner{
+
+    fn getMersOutput<'a>(&'a mut self, seed: u64, numInts: u32) -> Vec<u64> {
+        self.rng.reseed(seed);
+        let mut output=Vec::with_capacity(numInts);
+
+        for x in 0..numInts {
+            output.push(rng.next_u64());
+        }
+
+        return output;
+    }
+
+
+    fn sortedListsChal<'a>(&'a mut self, &mut numbers: Vec<u64>, order: F) -> String where F: FnMut(&T, &T) -> Ordering
+    {
+        //numbers.sort_by(order);
+
+        let results= numbers.iter().map(|x| x.ToString()).collect().join();
+
+        return results;
+
+    }
+
+
+}
 
 
