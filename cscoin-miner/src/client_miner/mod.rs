de650b8e112ebf63a21096fa927cd901f876c024
Alexis Grondin <agrondin1@hotmail.com>
2017-03-21 16:18:57 -0400

    Updated mod challenge_solver

--- cscoin-miner/src/client_miner/mod.rs
+++ cscoin-miner/src/client_miner/mod.rs
@@ -14,11 +14,14 @@ fn solve_challenge(current_challenge: CurrentChallenge)->Option<ChallengeSolutio
 
     match current_challenge.parameters{
         CurrentChallengeParams { grid_size: sz ref @ Some(_), nb_blockers: num ref @ Some(_), .. }=>{
-            miner.solve_shortest_path(current_challenge.last_solution_hash, sz, num);
+            return Some(miner.solve_shortest_path(current_challenge.last_solution_hash, sz, num));
         },
         CurrentChallengeParams { grid_size: None, nb_blockers: None, nb_elements: num_int ref @ some(_)}=>{
             let reverse = current_challenge.challenge_name=="reverse_sorted_list".toString();
-            miner.solve_order(current_challenge.last_solution_hash, num_int);
+            if reverse {return Some(miner.reverse_challenge(current_challenge.last_solution_hash, num_int));}
+            else {
+                return Some(miner.sorted_list_challenge(current_challenge.last_solution_hash, num_int));
+            }
         },
         _=>None
     }
